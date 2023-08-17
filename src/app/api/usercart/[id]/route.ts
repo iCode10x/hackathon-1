404e90174361318929d2a6f5c991fde1a132a888
Hashir Mahmood <hashirmahmood4270@gmail.com>
2023-08-17 16:56:48 +0500

    fixed an issue

--- src/app/api/usercart/[id]/route.ts
+++ src/app/api/usercart/[id]/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 import { db, cartTable } from '@/lib/drizzle'
 import { eq } from 'drizzle-orm'
 
 export const GET = async (
-  {},
+  req: NextRequest,
   { params: { id } }: { params: { id: string } }
 ) => {
   const res = await db.select().from(cartTable).where(eq(cartTable.userId, id))
